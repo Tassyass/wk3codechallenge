80c2fc9ec5e47a9c9d4b911e1990af90f730a53f
Abaadir <abaadir.yasin@student.moringaschool.com>
2023-10-21 21:52:27 +0300

    completed

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # wk3codechallenge
+## INTRODUCTION
+Film Listing and Ticket Purchase System
+This JavaScript application allows users to view a list of films, their details, and purchase tickets. It also shows whether a film is sold out or the number of remaining tickets.
+
+## Features
+
+Display a list of films with their titles, posters, showtimes, descriptions, and ticket availability.
+View detailed information about each film.
+Purchase tickets for available films.
+Show a "Sold Out" message if all tickets are sold, or display the number of remaining tickets after purchasing one.
+Getting Started
+Download the code files: index.html and script.js.
+
+Open the index.html file in a web browser to use the application.
+
+## How to Use
+On the main page, you will see a list of films. Each film will have a "View Details" and a "Buy Ticket" button.
+
+Click on the "View Details" button to see more information about a specific film, including its poster, runtime, showtime, description, and ticket availability.
+
+Click on the "Buy Ticket" button to purchase a ticket. The number of available tickets will decrease, and the number of remaining tickets will be displayed.
+
+If all tickets are sold out, a "Sold Out" message will be shown.
+
+## Code Structure
+index.html: The HTML file containing the structure of the web page.
+script.js: The JavaScript file responsible for data management, DOM manipulation, and event handling.
+Dependencies
+This application does not have any external dependencies. It runs in any modern web browser with JavaScript enabled.
+
+## License
+This project is licensed under the MIT License 
+
+Authors
+Tasniim Yasin
--- js/script.js
+++ js/script.js
@@ -3,234 +3,197 @@ const filmList = document.getElementById("film-list");
 const filmDetails = document.getElementById("film-details");
 
 //Sample film data
-// const films = [
-//     {
-//         "id": "1",
-//         "title": "The Giant Gila Monster",
-//         "runtime": "108",
-//         "capacity": 30,
-//         "showtime": "04:00PM",
-//         "tickets_sold": 27,
-//         "description": "A giant lizard terrorizes a rural Texas community and a heroic teenager attempts to destroy the creature.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg"
-//     },
-//     {
-//         "id": "2",
-//         "title": "Manos: The Hands Of Fate",
-//         "runtime": "118",
-//         "capacity": 50,
-//         "showtime": "06:45PM",
-//         "tickets_sold": 44,
-//         "description": "A family gets lost on the road and stumbles upon a hidden, underground, devil-worshiping cult led by the fearsome Master and his servant Torgo.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/47781/p47781_v_v8_ac.jpg"
-//       },
-//       {
-//         "id": "3",
-//         "title": "Time Chasers",
-//         "runtime": "93",
-//         "capacity": 50,
-//         "showtime": "09:30PM",
-//         "tickets_sold": 31,
-//         "description": "An inventor comes up with a time machine, but must prevent its abuse at the hands of an evil C.E.O.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/23342/p23342_v_v8_ab.jpg"
-//       },
-//       {
-//         "id": "4",
-//         "title": "The Touch Of Satan",
-//         "runtime": "101",
-//         "capacity": 40,
-//         "showtime": "09:00PM",
-//         "tickets_sold": 31,
-//         "description": "A young man meets a farm girl who is actually a witch.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/43468/p43468_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "5",
-//         "title": "Santa Claus Conquers The Martians",
-//         "runtime": "96",
-//         "capacity": 50,
-//         "showtime": "03:30PM",
-//         "tickets_sold": 45,
-//         "description": "The Martians kidnap Santa Claus because there is nobody on Mars to give their children presents.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/4232/p4232_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "6",
-//         "title": "Track Of The Moon Beast",
-//         "runtime": "112",
-//         "capacity": 30,
-//         "showtime": "10:30PM",
-//         "tickets_sold": 16,
-//         "description": "A young man is transformed into a hideous 'moon beast' due to a meteor fragment lodged in his body.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/39804/p39804_v_v8_ab.jpg"
-//       },
-//       {
-//         "id": "7",
-//         "title": "The Skydivers",
-//         "runtime": "94",
-//         "capacity": 30,
-//         "showtime": "07:30PM",
-//         "tickets_sold": 22,
-//         "description": "A woman seeks revenge on her former lover, who owns a skydiving business.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/40518/p40518_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "8",
-//         "title": "The Killer Shrews",
-//         "runtime": "115",
-//         "capacity": 40,
-//         "showtime": "08:30PM",
-//         "tickets_sold": 32,
-//         "description": "On an isolated island, a small group of people are terrorized by giant voracious shrews in the midst of a hurricane.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/1466/p1466_v_v8_ab.jpg"
-//       },
-//       {
-//         "id": "9",
-//         "title": "Project Moon Base",
-//         "runtime": "99",
-//         "capacity": 40,
-//         "showtime": "07:30PM",
-//         "tickets_sold": 22,
-//         "description": "A saboteur posing as a scientist strives to destroy the world's first space station.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/46755/p46755_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "10",
-//         "title": "The Giant Spider Invasion",
-//         "runtime": "122",
-//         "capacity": 50,
-//         "showtime": "10:00PM",
-//         "tickets_sold": 44,
-//         "description": "Giant spiders from another dimension invade Wisconsin.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/42171/p42171_v_v8_ab.jpg"
-//       },
-//       {
-//         "id": "11",
-//         "title": "Catalina Caper",
-//         "runtime": "104",
-//         "capacity": 30,
-//         "showtime": "06:00PM",
-//         "tickets_sold": 12,
-//         "description": "A group of swingin' teens take time out from having fun in the sun to try to foil a group of crooks searching for a stolen scroll.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/58122/p58122_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "12",
-//         "title": "Secret Agent Super Dragon",
-//         "runtime": "104",
-//         "capacity": 30,
-//         "showtime": "07:00PM",
-//         "tickets_sold": 20,
-//         "description": "A series of murders in Michigan lead an American secret agent to Amsterdam, where he uncovers a plot to imperil the world with a potent new drug.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/93417/p93417_v_v8_aa.jpg",
-//         "showing_id": "12"
-//       },
-//       {
-//         "id": "13",
-//         "title": "Wild Rebels",
-//         "runtime": "100",
-//         "capacity": 40,
-//         "showtime": "09:00PM",
-//         "tickets_sold": 31,
-//         "description": "A stock car driver goes undercover as the wheel man for a motorcycle gang.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/45367/p45367_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "14",
-//         "title": "Danger: Diabolik",
-//         "runtime": "111",
-//         "capacity": 50,
-//         "showtime": "08:00PM",
-//         "tickets_sold": 40,
-//         "description": "International man of mystery Diabolik and his lover pull off heist after heist, all while European cops led by Inspector Ginko and envious mobsters led by Ralph Valmont are closing in on them.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/11850/p11850_v_v8_aa.jpg"
-//       },
-//       {
-//         "id": "15",
-//         "title": "Village Of The Giants",
-//         "runtime": "98",
-//         "capacity": 50,
-//         "showtime": "04:45PM",
-//         "tickets_sold": 33,
-//         "description": "Delinquent teen-agers ingest a substance and grow thirty feet tall, then proceed to take over a small town.",
-//         "poster": "https://www.gstatic.com/tv/thumb/v22vodart/37991/p37991_v_v8_aa.jpg"
-//       }
-//     ];
-
-
-
-const baseURL= 'http://localhost:3000/films'
-fetch(baseURL)
-.then (res => res.json())
-.then(data => {populateFilmList(data)})
-.catch(error => console.log(error))
-
-
+const films = [
+    {
+        "id": "1",
+        "title": "The Giant Gila Monster",
+        "runtime": "108",
+        "capacity": 30,
+        "showtime": "04:00PM",
+        "tickets_sold": 27,
+        "description": "A giant lizard terrorizes a rural Texas community and a heroic teenager attempts to destroy the creature.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Manos: The Hands Of Fate",
+        "runtime": "118",
+        "capacity": 50,
+        "showtime": "06:45PM",
+        "tickets_sold": 44,
+        "description": "A family gets lost on the road and stumbles upon a hidden, underground, devil-worshiping cult led by the fearsome Master and his servant Torgo.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/47781/p47781_v_v8_ac.jpg"
+      },
+      {
+        "id": "3",
+        "title": "Time Chasers",
+        "runtime": "93",
+        "capacity": 50,
+        "showtime": "09:30PM",
+        "tickets_sold": 31,
+        "description": "An inventor comes up with a time machine, but must prevent its abuse at the hands of an evil C.E.O.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/23342/p23342_v_v8_ab.jpg"
+      },
+      {
+        "id": "4",
+        "title": "The Touch Of Satan",
+        "runtime": "101",
+        "capacity": 40,
+        "showtime": "09:00PM",
+        "tickets_sold": 31,
+        "description": "A young man meets a farm girl who is actually a witch.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/43468/p43468_v_v8_aa.jpg"
+      },
+      {
+        "id": "5",
+        "title": "Santa Claus Conquers The Martians",
+        "runtime": "96",
+        "capacity": 50,
+        "showtime": "03:30PM",
+        "tickets_sold": 45,
+        "description": "The Martians kidnap Santa Claus because there is nobody on Mars to give their children presents.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/4232/p4232_v_v8_aa.jpg"
+      },
+      {
+        "id": "6",
+        "title": "Track Of The Moon Beast",
+        "runtime": "112",
+        "capacity": 30,
+        "showtime": "10:30PM",
+        "tickets_sold": 16,
+        "description": "A young man is transformed into a hideous 'moon beast' due to a meteor fragment lodged in his body.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/39804/p39804_v_v8_ab.jpg"
+      },
+      {
+        "id": "7",
+        "title": "The Skydivers",
+        "runtime": "94",
+        "capacity": 30,
+        "showtime": "07:30PM",
+        "tickets_sold": 22,
+        "description": "A woman seeks revenge on her former lover, who owns a skydiving business.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/40518/p40518_v_v8_aa.jpg"
+      },
+      {
+        "id": "8",
+        "title": "The Killer Shrews",
+        "runtime": "115",
+        "capacity": 40,
+        "showtime": "08:30PM",
+        "tickets_sold": 32,
+        "description": "On an isolated island, a small group of people are terrorized by giant voracious shrews in the midst of a hurricane.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/1466/p1466_v_v8_ab.jpg"
+      },
+      {
+        "id": "9",
+        "title": "Project Moon Base",
+        "runtime": "99",
+        "capacity": 40,
+        "showtime": "07:30PM",
+        "tickets_sold": 22,
+        "description": "A saboteur posing as a scientist strives to destroy the world's first space station.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/46755/p46755_v_v8_aa.jpg"
+      },
+      {
+        "id": "10",
+        "title": "The Giant Spider Invasion",
+        "runtime": "122",
+        "capacity": 50,
+        "showtime": "10:00PM",
+        "tickets_sold": 44,
+        "description": "Giant spiders from another dimension invade Wisconsin.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/42171/p42171_v_v8_ab.jpg"
+      },
+      {
+        "id": "11",
+        "title": "Catalina Caper",
+        "runtime": "104",
+        "capacity": 30,
+        "showtime": "06:00PM",
+        "tickets_sold": 12,
+        "description": "A group of swingin' teens take time out from having fun in the sun to try to foil a group of crooks searching for a stolen scroll.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/58122/p58122_v_v8_aa.jpg"
+      },
+      {
+        "id": "12",
+        "title": "Secret Agent Super Dragon",
+        "runtime": "104",
+        "capacity": 30,
+        "showtime": "07:00PM",
+        "tickets_sold": 20,
+        "description": "A series of murders in Michigan lead an American secret agent to Amsterdam, where he uncovers a plot to imperil the world with a potent new drug.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/93417/p93417_v_v8_aa.jpg",
+        "showing_id": "12"
+      },
+      {
+        "id": "13",
+        "title": "Wild Rebels",
+        "runtime": "100",
+        "capacity": 40,
+        "showtime": "09:00PM",
+        "tickets_sold": 31,
+        "description": "A stock car driver goes undercover as the wheel man for a motorcycle gang.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/45367/p45367_v_v8_aa.jpg"
+      },
+      {
+        "id": "14",
+        "title": "Danger: Diabolik",
+        "runtime": "111",
+        "capacity": 50,
+        "showtime": "08:00PM",
+        "tickets_sold": 40,
+        "description": "International man of mystery Diabolik and his lover pull off heist after heist, all while European cops led by Inspector Ginko and envious mobsters led by Ralph Valmont are closing in on them.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/11850/p11850_v_v8_aa.jpg"
+      },
+      {
+        "id": "15",
+        "title": "Village Of The Giants",
+        "runtime": "98",
+        "capacity": 50,
+        "showtime": "04:45PM",
+        "tickets_sold": 33,
+        "description": "Delinquent teen-agers ingest a substance and grow thirty feet tall, then proceed to take over a small town.",
+        "poster": "https://www.gstatic.com/tv/thumb/v22vodart/37991/p37991_v_v8_aa.jpg"
+      }
+    ];
 
 // // Function to populate the film list
-function populateFilmList(data) {
-    data.forEach((film) => {
-        const filmElement = document.createElement("div");
-        filmElement.className = "film";
-        filmElement.innerHTML = `
-            <h3>${film.title}</h3>
-            <img src="${film.poster}" alt="${film.title}">
-            <p>Showtime: ${film.showtime}</p>
-            <p>Description: ${film.description}</p>
-            <p>Tickets Sold: ${film.tickets_sold} / Capacity: ${film.capacity}</p>
-            <button id="${film.id}" class="view-details">View Details</button>
-            <button id="${film.id}" class="buy-ticket">Buy Ticket</button>
-        `;
-        filmList?.appendChild(filmElement);
-    });
-}
+// ... (previous code)
 
-// // Event listener for viewing film details
-filmList?.addEventListener("click", (event) => {
-    if (event.target.classList.contains("view-details")) {
-        const filmId = event.target.id;
-        const selectedFilm = films.find((film) => film.id === filmId);
-        showFilmDetails(selectedFilm);
-    }
-});
-
-// // Event listener for buying a ticket
-filmList?.addEventListener("click", (event) => {
-    if (event.target.classList.contains("buy-ticket")) {
-        const filmId = event.target.id;
-        const selectedFilm = films.find((film) => film.id === filmId);
-        buyTicket(selectedFilm);
-    }
-});
-
-// // Function to display film details
+// Function to display film details
 function showFilmDetails(film) {
-    filmDetails.innerHTML = `
-        <h2>${film.title}</h2>
-        <img src="${film.poster}" alt="${film.title}">
-        <p>Runtime: ${film.runtime} minutes</p>
-        <p>Showtime: ${film.showtime}</p>
-        <p>Tickets Sold: ${film.tickets_sold} / Capacity: ${film.capacity}</p>
-        <p>Description: ${film.description}</p>
-    `;
+  filmDetails.innerHTML = `
+      <h2>${film.title}</h2>
+      <img src="${film.poster}" alt="${film.title}">
+      <p>Runtime: ${film.runtime} minutes</p>
+      <p>Showtime: ${film.showtime}</p>
+      <p>Tickets Sold: ${film.tickets_sold} / Capacity: ${film.capacity}</p>
+      <p>Description: ${film.description}</p>
+      <p>Remaining Tickets: ${film.capacity - film.tickets_sold}</p>
+  `;
 
-    
-    if (film.tickets_sold >= film.capacity) {
-        filmDetails.innerHTML += '<p class="sold-out">Sold Out</p>';
-    }
+  if (film.tickets_sold >= film.capacity) {
+      filmDetails.innerHTML += '<p class="sold-out">Sold Out</p>';
+  }
 
-    filmDetails.style.display = "block";
-    filmList.style.display = "none";
+  filmDetails.style.display = "block";
+  filmList.style.display = "none";
 }
 
-// // Function to buy a ticket
+// Function to buy a ticket
 function buyTicket(film) {
-    if (film.tickets_sold < film.capacity) {
-        film.tickets_sold++;
-        showFilmDetails(film); // Update film details
-    } else {
-        filmDetails.innerHTML += '<p class="sold-out">Sold Out</p>';
-    }
+  if (film.tickets_sold < film.capacity) {
+      film.tickets_sold++;
+      showFilmDetails(film); // Update film details
+  } else {
+      showFilmDetails(film); // Movie is sold out
+  }
+}
+
+// Function to buy a ticket
+function buyTicket(film) {
+  film.tickets_sold++;
+  showFilmDetails(film); // Update film details
 }
 
 // Initialize the film list
